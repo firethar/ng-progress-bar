202a921a39186a6f520890329978638c979621c8
Wojciech Błażejewski <firethar@gmail.com>
2021-02-07 19:58:43 +0100

    chore: progress-bar set as dummy component with input

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
-import { SoldProducts } from '../product';
-import { ProductService } from '../product.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -9,40 +6,19 @@ import { ProductService } from '../product.service';
   styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent implements OnInit {
-  progressValue: number = 0;
+  _progressValue: number | null = 0;
+  
+  @Input() set progressValue(value: number | null) {
+    this._progressValue = value;
+  }
+  
+  get progressValue(): number | null {
+    return this._progressValue;
+  }
 
-  constructor(private productService: ProductService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getSoldProducts();
-  }
-
-  processData(soldProducts: SoldProducts):void {
-    let total: number = 0 ;
-    soldProducts.totalValue;
-    soldProducts.data.forEach( product => {
-      return total += product.value;
-    });
-
-    this.showProgress(this.calculateProgress(total, soldProducts.totalValue));
-  }
-
-  getSoldProducts(): void {
-    this.productService.getSoldProducts()
-      .subscribe(val => this.processData(val));
-  }
-
-  calculateProgress(total: number, targetValue: number): number {
-    const calculatedProgress = Math.round(100 * total / targetValue);
-    if (calculatedProgress < 0 ) {
-      return 0;
-    } else {
-      return calculatedProgress > 100 ? 100 : calculatedProgress;
-    }
-  }
-
-  showProgress(value: number): void {
-    this.progressValue = value;
   }
 
 }
