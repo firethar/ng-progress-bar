e45ce2d6af954b64ad97209e2dfbe3baaae13e6a
Wojciech Błażejewski <firethar@gmail.com>
2021-02-07 15:58:49 +0100

    add: cached data to prevent multiple api requests

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map, publishReplay, refCount } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
@@ -12,11 +13,29 @@ import { SoldProducts } from './product';
 export class ProductService {
   private productsURL = `${environment.baseURL}/soldProducts`; 
 
+  productsData$?: Observable<SoldProducts>;
+
   constructor(private http: HttpClient) { }
 
   /** GET soldProducts from the server */
   getSoldProducts(): Observable<SoldProducts> {
-    return this.http.get<SoldProducts>(this.productsURL);
+    // Cache it once if productsData$ value is false
+    if (!this.productsData$) {
+      this.productsData$ = this.http.get<SoldProducts>(this.productsURL).pipe(
+        publishReplay(1),
+        refCount()
+      );
+    }
+    return this.productsData$;
+
+  }
+
+  getTargetValue(): Observable<number> {
+    return this.getSoldProducts().pipe(
+      map((val: SoldProducts): number => {
+        return val.totalValue;
+      })
+    );
   }
 
 }
