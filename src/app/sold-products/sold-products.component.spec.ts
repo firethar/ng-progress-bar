a5544fb2edbf701f8bfbb9071ee86564384b43f2
Wojciech Błażejewski <firethar@gmail.com>
2021-02-09 00:15:36 +0100

    add: unit tests for sold-product component

--- src/app/sold-products/sold-products.component.spec.ts
+++ src/app/sold-products/sold-products.component.spec.ts
@@ -1,14 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductService } from '../product.service';
 
 import { SoldProductsComponent } from './sold-products.component';
 
 describe('SoldProductsComponent', () => {
   let component: SoldProductsComponent;
   let fixture: ComponentFixture<SoldProductsComponent>;
+  let service: ProductService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SoldProductsComponent ]
+      imports: [
+        HttpClientModule
+      ],
+      declarations: [ SoldProductsComponent ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
     })
     .compileComponents();
   });
@@ -17,9 +27,29 @@ describe('SoldProductsComponent', () => {
     fixture = TestBed.createComponent(SoldProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.inject(ProductService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+ 
+  it('service should be injected', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get products$ Observable', (done) => {
+    component.products$.subscribe(value => {
+      expect(value).toBeTruthy();
+      done();
+    });
+  });
+
+  it('should get targetValue$ Observable', (done) => {
+    component.targetValue$.subscribe(value => {
+      expect(value).toBeTruthy();
+      done();
+    });
+  });
+
 });
